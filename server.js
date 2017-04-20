6de6a5e3db7bc0b352b3a0849b409a0be7abba4c
Ramon Pelkmans <rrpelkma@avans.nl>
2017-04-20 12:21:13 +0200

    tekst aangepast

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ http.createServer(function(request, response){
 	console.log('Er was een request.');
 	response.writeHead(200, {'Content-Type': 'text/json'});
 	var json = JSON.stringify({
-		tekst: "Dit is saai"
+		tekst: "Ik ben verliefd! x ramon"
 	});
 	response.end(json);
 }).listen(process.env.PORT || 3000);
